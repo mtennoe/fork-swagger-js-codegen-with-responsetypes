da9c6a3186fc6054665d922bf1f203df6860565a
Michał Zarach <michal.m.zarach@gmail.com>
2019-04-03 14:40:17 +0200

    fix cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,5 +1,7 @@
 import * as fs from "fs";
 import * as cli from "commander";
+
+import * as path from "path";
 import { CodeGen } from "./codegen";
 
 const pkg = require("../package.json");
@@ -7,11 +9,14 @@ const pkg = require("../package.json");
 cli
   .command("generate <file>")
   .description("Generate from Swagger file")
-  .action((file: string) => {
+  .option("-o, --out [outFile]", "Output file")
+  .action((file: string, options: any) => {
     const result = CodeGen.generateCode({
       swagger: JSON.parse(fs.readFileSync(file, "utf-8"))
     });
-    console.log(result);
+    const outFileName = options.out ? options.out : "./api.ts";
+    const outFilePath = path.join(process.cwd(), outFileName);
+    fs.writeFileSync(outFilePath, result, { encoding: "UTF-8" });
   });
 
 cli.version(pkg.version);
